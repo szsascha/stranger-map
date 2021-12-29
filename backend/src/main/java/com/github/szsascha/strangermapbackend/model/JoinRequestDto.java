bf32b2c41769abb40ee13804d013a23fa7f44293
SaschaS93 <43013380+SaschaS93@users.noreply.github.com>
2021-12-29 14:48:14 +0100

    Add size restrictions

--- backend/src/main/java/com/github/szsascha/strangermapbackend/model/JoinRequestDto.java
+++ backend/src/main/java/com/github/szsascha/strangermapbackend/model/JoinRequestDto.java
@@ -3,14 +3,17 @@ package com.github.szsascha.strangermapbackend.model;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public class JoinRequestDto {
 
     @NotBlank
+    @Size(min = 1, max = 20)
     private String name;
 
     @NotBlank
+    @Size(min = 1, max = 500)
     private String description;
 
 }
