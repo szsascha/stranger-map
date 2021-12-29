3dbdd4e24019ce36095a8455cc6c126d45399c94
SaschaS93 <43013380+SaschaS93@users.noreply.github.com>
2021-12-29 12:39:24 +0100

    Add validation

--- backend/src/main/java/com/github/szsascha/strangermapbackend/controller/SessionController.java
+++ backend/src/main/java/com/github/szsascha/strangermapbackend/controller/SessionController.java
@@ -6,11 +6,13 @@ import com.github.szsascha.strangermapbackend.service.PeerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -22,7 +24,7 @@ public class SessionController {
     private PeerService peerService;
 
     @PostMapping
-    ResponseEntity<JoinResponseDto> join(@RequestBody JoinRequestDto joinRequest) {
+    ResponseEntity<JoinResponseDto> join(@Valid @RequestBody JoinRequestDto joinRequest) {
         final UUID uuid = peerService.register(joinRequest.getName(), joinRequest.getDescription());
         log.info("Peer {}, ({}) registered with UUID {}", joinRequest.getName(), joinRequest.getDescription(), uuid);
 
--- backend/src/main/java/com/github/szsascha/strangermapbackend/model/JoinRequestDto.java
+++ backend/src/main/java/com/github/szsascha/strangermapbackend/model/JoinRequestDto.java
@@ -1,12 +1,17 @@
 package com.github.szsascha.strangermapbackend.model;
 
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class JoinRequestDto {
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private String description;
 
 }
